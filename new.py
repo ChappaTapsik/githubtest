4cbffb7a4d7d91613228c60536e61cdce79fbbdf
ChappaTapsik <94403308+ChappaTapsik@users.noreply.github.com>
2021-12-19 09:58:33 +0100

    Add files via upload

---
+++ new.py
@@ -0,0 +1,30 @@
+write=""
+started = False
+
+while 1==1:
+    write=input("> ").lower()
+
+    if write =="help":
+        print("""
+start - to start the car
+stop - to stop the car
+quit - to exit""")
+    elif write=="start":
+        if started:
+            print("Car already started")
+
+        else:
+            print("Car started...ready to go!")
+            started=True
+    elif write=="stop":
+        if not started==True:
+            print("Car is already stopped")
+        else:
+            print("Car stopped")
+            started = False
+    elif write=="quit":
+
+        break
+
+    else:
+        print("I don't understand")
